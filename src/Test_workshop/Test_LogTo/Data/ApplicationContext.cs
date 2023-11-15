31e02b80dafc764840697d3f6fc68f1ef47527a3
Alexander <sany199574@gmail.com>
2023-11-15 13:13:12 +0300

    Добавлена реализация логирования через запись в файл

---
+++ src/Test_workshop/Test_LogTo/Data/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_LogTo.Models;
+
+namespace Test_LogTo.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly StreamWriter logStream = new StreamWriter("mylog.txt", true);
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test_BaseProject_DB;Trusted_Connection=True;");
+            optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name });
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            logStream.Dispose();
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await logStream.DisposeAsync();
+        }
+    }
+}
---
+++ src/Test_workshop/Test_LogTo/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using Test_LogTo.Data;
+using Test_LogTo.Models;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    User user1 = new User { Name = "Tom", Age = 33 };
+    User user2 = new User { Name = "Alice", Age = 26 };
+
+    db.Users.Add(user1);
+    db.Users.Add(user2);
+    db.SaveChanges();
+
+    var users = db.Users.ToList();
+    Console.WriteLine("Данные после добавления:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
+Console.Read();
