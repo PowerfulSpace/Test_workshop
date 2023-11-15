5dea0bce894f0564f5773e06149797530f1cd65f
Alexander <sany199574@gmail.com>
2023-11-15 18:40:05 +0300

    Добавлен тестовф пример, использщование миграций

---
+++ src/Test_workshop/Test_scheme/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_scheme.Models;
+
+namespace Test_scheme.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test_BaseProject_DB;Trusted_Connection=True;");
+        }
+
+    }
+}
---
+++ src/Test_workshop/Test_scheme/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using Test_scheme.Data;
+using Test_scheme.Models;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    var users = db.Users.ToList();
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
+
+
+Console.ReadLine();
+
+//https://metanit.com/sharp/entityframeworkcore/2.4.php
