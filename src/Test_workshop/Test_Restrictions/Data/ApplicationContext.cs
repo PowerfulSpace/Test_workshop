04c45797be5a241316281a3407b6f4bc71503364
Alexander <sany199574@gmail.com>
2023-11-17 17:01:30 +0300

    Добавлен проект, с работой ограничений свойств

---
+++ src/Test_workshop/Test_Restrictions/Data/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_Restrictions.Models;
+
+namespace Test_Restrictions.Data
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Test_BaseProject_DB;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().Property(x => x.Name).IsRequired();
+        }
+    }
+}
---
+++ src/Test_workshop/Test_Restrictions/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Test_Restrictions.Data;
+using Test_Restrictions.Models;
+
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("Список объектов:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name}");
+    }
+}
+Console.ReadKey();
+
+
+
