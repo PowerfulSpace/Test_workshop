f4925c9145afae69e1e916569879ef9b23003147
Alexander <sany199574@gmail.com>
2023-11-18 23:34:15 +0300

    Добавлен пример, гд тут используется явнаявяная загрузка

---
+++ src/Test_workshop/Test_Explicit_Loading/Data/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_Explicit_Loading.Models;
+
+namespace Test_Explicit_Loading.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext()
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Test_BaseProject_DB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ src/Test_workshop/Test_Explicit_Loading/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Test_Explicit_Loading.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int? CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
