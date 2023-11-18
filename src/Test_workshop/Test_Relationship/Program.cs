be25f14239dc0aaf275e927741990987c34d14fc
Alexander <sany199574@gmail.com>
2023-11-18 03:07:02 +0300

    Добавлена ссылка на сайт

--- src/Test_workshop/Test_Relationship/Program.cs
+++ src/Test_workshop/Test_Relationship/Program.cs
@@ -3,32 +3,14 @@ using Test_Relationship.Data;
 using Test_Relationship.Models;
 
 
-using (ApplicationContext db = new ApplicationContext())
-{
-    Company company1 = new Company { Name = "Google" };
-    Company company2 = new Company { Name = "Microsoft" };
-
-    User user1 = new User { Name = "Tom", Company = company1 };
-    User user2 = new User { Name = "Bob", Company = company2 };
-    User user3 = new User { Name = "Sam", Company = company2 };
-
-    db.Companies.AddRange(company1, company2);  // добавление компаний
-    db.Users.AddRange(user1, user2, user3);     // добавление пользователей
-    db.SaveChanges();
-
-    foreach (var user in db.Users.ToList())
-    {
-        Console.WriteLine($"{user.Name} работает в {user.Company?.Name}");
-    }
-}
 
 using (ApplicationContext db = new ApplicationContext())
 {
     Company company1 = new Company { Name = "Google" };
     Company company2 = new Company { Name = "Microsoft" };
-    User user1 = new User { Name = "Tom", Company = company1 };
-    User user2 = new User { Name = "Bob", Company = company2 };
-    User user3 = new User { Name = "Sam", Company = company2 };
+    User user1 = new User { Name = "Tom", CompanyName = company1.Name };
+    User user2 = new User { Name = "Bob", CompanyName = "Microsoft" };
+    User user3 = new User { Name = "Sam", CompanyName = company2.Name };
 
     db.Companies.AddRange(company1, company2);  // добавление компаний
     db.Users.AddRange(user1, user2, user3);     // добавление пользователей
@@ -42,7 +24,7 @@ using (ApplicationContext db = new ApplicationContext())
 
 Console.ReadKey();
 
-//https://metanit.com/sharp/entityframeworkcore/3.1.php
+//https://metanit.com/sharp/entityframeworkcore/3.2.php
 
 
 
