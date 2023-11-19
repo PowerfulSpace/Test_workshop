330105c8a37fb68af13e656b31ea1e36f77eb930
Alexander <sany199574@gmail.com>
2023-11-19 21:46:54 +0300

    Добавлен проект, связь многие ко многтим

---
+++ src/Test_workshop/Test_Many_To_Many/Data/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test_Many_To_Many.Models;
+
+namespace Test_Many_To_Many.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        public ApplicationContext()
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Test_BaseProject_DB;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
---
+++ src/Test_workshop/Test_Many_To_Many/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace Test_Many_To_Many.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Course> Courses { get; set; } = new List<Course>();
+    }
+}
