e4e0bb48a0a79d90cd78c569fc13adaaa4cd560b
Alexander <sany199574@gmail.com>
2023-11-21 00:58:32 +0300

    Добавлен пустой проект. Для реализации связей owner

---
+++ src/Test_workshop/Test_Owner/Data/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Test_Owner.Data
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public ApplicationContext()
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Test_BaseProject_DB;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
