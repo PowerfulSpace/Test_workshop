894b0d917859dd151e616b9027b4ec3b10d5fb99
Alexander <sany199574@gmail.com>
2023-11-22 02:47:06 +0300

    Реализован пример в котором применяется Подход TPT - Table Per Type

---
+++ src/Test_workshop/Test_TPT/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test_TPT.Models
+{
+    [Table("Employees")]
+    public class Employee : User
+    {
+        public int Salary { get; set; }
+    }
+}
---
+++ src/Test_workshop/Test_TPT/Models/Manager.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test_TPT.Models
+{
+    [Table("Managers")]
+    public class Manager : User
+    {
+        public string Departament { get; set; }
+    }
+}
