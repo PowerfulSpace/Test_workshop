ce9e7cabbc571615e90f10324ad21bde6b1d2f5e
Alexander <sany199574@gmail.com>
2023-11-23 00:38:52 +0300

    Добавлен пример группировки

--- src/Test_workshop/Test_LINQ_To_Entities/Program.cs
+++ src/Test_workshop/Test_LINQ_To_Entities/Program.cs
@@ -1,41 +1,42 @@
 ﻿using Test_LINQ_To_Entities.Data;
 using Test_LINQ_To_Entities.Models;
 
+//using (ApplicationContext db = new ApplicationContext())
+//{
+//    // пересоздаем базу данных
+//    db.Database.EnsureDeleted();
+//    db.Database.EnsureCreated();
+
+//    Country usa = new Country { Name = "USA" };
+
+//    Company microsoft = new Company { Name = "Microsoft", Country = usa };
+//    Company google = new Company { Name = "Google", Country = usa };
+//    db.Companies.AddRange(microsoft, google);
+
+//    User tom = new User { Name = "Tom", Age = 36, Company = microsoft };
+//    User bob = new User { Name = "Bob", Age = 39, Company = google };
+//    User alice = new User { Name = "Alice", Age = 28, Company = microsoft };
+//    User kate = new User { Name = "Kate", Age = 25, Company = google };
+
+//    db.Users.AddRange(tom, bob, alice, kate);
+//    db.SaveChanges();
+//}
+
 using (ApplicationContext db = new ApplicationContext())
 {
-    // пересоздаем базу данных
-    db.Database.EnsureDeleted();
-    db.Database.EnsureCreated();
+    var groups = from u in db.Users
+                 group u by u.Company.Name into g
+                 select new
+                 {
+                     g.Key,
+                     Count = g.Count()
+                 };
 
-    Country usa = new Country { Name = "USA" };
 
-    Company microsoft = new Company { Name = "Microsoft", Country = usa };
-    Company google = new Company { Name = "Google", Country = usa };
-    db.Companies.AddRange(microsoft, google);
-
-    User tom = new User { Name = "Tom", Age = 36, Company = microsoft };
-    User bob = new User { Name = "Bob", Age = 39, Company = google };
-    User alice = new User { Name = "Alice", Age = 28, Company = microsoft };
-    User kate = new User { Name = "Kate", Age = 25, Company = google };
-
-    db.Users.AddRange(tom, bob, alice, kate);
-    db.SaveChanges();
-}
-
-using (ApplicationContext db = new ApplicationContext())
-{
-    var users = from user in db.Users
-                join company in db.Companies on user.CompanyId equals company.Id
-                join country in db.Countries on company.CountryId equals country.Id
-                select new
-                {
-                    Name = user.Name,
-                    Company = company.Name,
-                    Age = user.Age,
-                    Country = country.Name
-                };
-    foreach (var u in users)
-        Console.WriteLine($"{u.Name} ({u.Company} - {u.Country}) - {u.Age}");
+    foreach (var group in groups)
+    {
+        Console.WriteLine($"{group.Key} - {group.Count}");
+    }
 }
 
 Console.ReadLine();
