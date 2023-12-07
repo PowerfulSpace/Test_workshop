c8529d7d772cc45011963276fee6ecd521e1cda3
Alexander <sany199574@gmail.com>
2023-12-07 17:51:59 +0300

    Добаблены конфигурации  - MovieConfiguration  - GenreConfiguration

---
+++ src/Test_workshop/DisignProject/Data/Configurations/GenreConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using DisignProject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DisignProject.Data.Configurations
+{
+    public class GenreConfiguration : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            builder
+              .HasMany(x => x.Movies)
+              .WithMany(x => x.Genres);
+
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(25);
+            builder.Property(x => x.Description).IsRequired();
+        }
+    }
+}
