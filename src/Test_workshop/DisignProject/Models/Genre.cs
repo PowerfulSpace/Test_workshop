e8fe5395c89e19e71e7d997c7d11a63e06140316
Alexander <sany199574@gmail.com>
2023-12-07 17:45:01 +0300

    Добавлено 2 модели -Gener -Movie

---
+++ src/Test_workshop/DisignProject/Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace DisignProject.Models
+{
+    public class Genre
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+
+        public List<Movie> Movies { get; set; } = new List<Movie>();
+    }
+}
---
+++ src/Test_workshop/DisignProject/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DisignProject.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; } = null!;
+
+        public List<Genre> Genres { get; set; } = new List<Genre>();
+    }
+}
