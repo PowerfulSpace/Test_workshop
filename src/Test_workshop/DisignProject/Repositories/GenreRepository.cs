048b77935de4760fab348d6f57eac91d211a1fda
Alexander <sany199574@gmail.com>
2023-12-07 18:08:08 +0300

    Добавлено 2 репозитория, не реализованных - MovieRepository - GenreRepository

---
+++ src/Test_workshop/DisignProject/Repositories/GenreRepository.cs
@@ -0,0 +1,43 @@
+﻿using DisignProject.Interfaces;
+using DisignProject.Models;
+
+namespace DisignProject.Repositories
+{
+    public class GenreRepository : IGenre
+    {
+        public Task<Genre> DeleteAsync(Genre item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Genre> EditAsync(Genre item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Genre> GetItemAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Genre>> GetItemsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Genre> GreateAsync(Genre item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsItemNameExists(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsItemNameExists(string name, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Test_workshop/DisignProject/Repositories/MovieRepository.cs
@@ -0,0 +1,43 @@
+﻿using DisignProject.Interfaces;
+using DisignProject.Models;
+
+namespace DisignProject.Repositories
+{
+    public class MovieRepository : IMovie
+    {
+        public Task<Movie> DeleteAsync(Movie item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> EditAsync(Movie item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetItemAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Movie>> GetItemsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GreateAsync(Movie item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsItemNameExists(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsItemNameExists(string name, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
