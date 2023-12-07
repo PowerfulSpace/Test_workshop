497caa54353b07317205529bf1c7c372ae65f890
Alexander <sany199574@gmail.com>
2023-12-07 17:48:59 +0300

    Добавлена конфигурация  - ApplicationContext

---
+++ src/Test_workshop/DisignProject/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using DisignProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DisignProject.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; } = null!;
+        public DbSet<Genre> Genres { get; set; } = null!;
+    }
+}
