5f8ef87e955e2aabe7c7fbed2c52bc626da30509
Alexander <sany199574@gmail.com>
2023-12-07 18:06:34 +0300

    Добавлены интерфейсы - IGenre - Movie

---
+++ src/Test_workshop/DisignProject/Interfaces/IGenre.cs
@@ -0,0 +1,11 @@
+﻿using DisignProject.Interfaces.Base;
+using DisignProject.Models;
+
+namespace DisignProject.Interfaces
+{
+    public interface IGenre : IBaseRepository<Genre>
+    {
+        public bool IsItemNameExists(string name);
+        public bool IsItemNameExists(string name, Guid id);
+    }
+}
---
+++ src/Test_workshop/DisignProject/Interfaces/IMovie.cs
@@ -0,0 +1,11 @@
+﻿using DisignProject.Interfaces.Base;
+using DisignProject.Models;
+
+namespace DisignProject.Interfaces
+{
+    public interface IMovie : IBaseRepository<Movie>
+    {
+        public bool IsItemNameExists(string name);
+        public bool IsItemNameExists(string name, Guid id);
+    }
+}
