c075f3e65cf70e73125705b6bcea3a3c5ac28679
Alexander <sany199574@gmail.com>
2023-12-09 21:11:02 +0300

    Добавлен тестовый проект - Test_Difficult_Multiselect

---
+++ src/Test_workshop/Test_Difficult_Multiselect/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+DefaultFilesOptions options = new DefaultFilesOptions();
+options.DefaultFileNames.Clear(); // удаляем имена файлов по умолчанию
+options.DefaultFileNames.Add("index.html"); // добавляем новое имя файла
+app.UseDefaultFiles(options); // установка параметров
+
+
+app.UseStaticFiles();
+
+app.Run(async (context) => await context.Response.WriteAsync("Hello World"));
+
+app.Run();
