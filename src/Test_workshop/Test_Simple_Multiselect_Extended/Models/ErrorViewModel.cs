9dca31ea7be65e084cc699864fdf4aefacc6221c
Alexander <sany199574@gmail.com>
2023-12-09 20:56:07 +0300

    Реализация выпадающего списка, с полномосштабным проектом

---
+++ src/Test_workshop/Test_Simple_Multiselect_Extended/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_Simple_Multiselect_Extended.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
