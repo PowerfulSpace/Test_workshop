c91c5be0f5b7ac695419b8fd04c02150189768df
Alexander <sany199574@gmail.com>
2023-12-09 20:37:45 +0300

    реализован Test_Simple_Multiselect

--- src/Test_workshop/Test_Simple_Multiselect/Program.cs
+++ src/Test_workshop/Test_Simple_Multiselect/Program.cs
@@ -1,6 +1,17 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+DefaultFilesOptions options = new DefaultFilesOptions();
+options.DefaultFileNames.Clear(); // удаляем имена файлов по умолчанию
+options.DefaultFileNames.Add("index.html"); // добавляем новое имя файла
+app.UseDefaultFiles(options); // установка параметров
+
+
+app.UseStaticFiles();
+
+app.Run(async (context) => await context.Response.WriteAsync("Hello World"));
 
 app.Run();
