d7758499783e5dd8f6ca937e86aca3ba963d2740
Alexander <sany199574@gmail.com>
2023-12-27 18:45:11 +0300

    Добавлено подключени Добавление элемента вывод элемента

--- src/Test_workshop/ConnectMongoDB/Models/PersonModel.cs
+++ src/Test_workshop/ConnectMongoDB/Models/PersonModel.cs
@@ -1,6 +1,16 @@
-﻿namespace ConnectMongoDB.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ConnectMongoDB.Models
 {
     public class PersonModel
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
--- src/Test_workshop/ConnectMongoDB/Program.cs
+++ src/Test_workshop/ConnectMongoDB/Program.cs
@@ -1,8 +1,31 @@
 ﻿
 
 
-Console.WriteLine("Hello, World!");
+using ConnectMongoDB.Models;
+using MongoDB.Driver;
 
+string connectionString = "mongodb://127.0.0.1:27017";
+string databaseName = "simple_db";
+string collectionName = "people";
+
+MongoClient client = new MongoClient(connectionString);
+IMongoDatabase db = client.GetDatabase(databaseName);
+IMongoCollection<PersonModel> collection = db.GetCollection<PersonModel>(collectionName);
+
+var person = new PersonModel { FirstName = "Tim", LastName = "Corey" };
+await collection.InsertOneAsync(person);
+
+//Возвращает все записи
+var results = await collection.FindAsync(_ => true);
+
+foreach (var result in results.ToList())
+{
+    Console.WriteLine($"{result.Id}:{result.FirstName}:{result.LastName}");
+}
+
+
+
+Console.ReadLine();
 
 //https://www.youtube.com/watch?v=exXavNOqaVo&ab_channel=IAmTimCorey
 
