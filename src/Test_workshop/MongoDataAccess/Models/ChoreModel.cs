81c1e826d3f972ca123a604a89eb3e35645b9614
Alexander <sany199574@gmail.com>
2023-12-27 19:03:46 +0300

    реализованы поля в модели  - ChoreModel

---
+++ src/Test_workshop/MongoDataAccess/Models/ChoreModel.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MongoDataAccess.Models
+{
+    public class ChoreModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+
+        public string ChoreText { get; set; }
+        public int FrequencyInDays { get; set; }
+        public UserModel? AssignedTo { get; set; }
+        public DateTime? LastComleted { get; set; }
+    }
+}
