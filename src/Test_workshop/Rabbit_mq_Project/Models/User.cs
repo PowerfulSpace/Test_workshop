58dc991b9bb3f13bfb189dda3eb91597f5a4e38e
Alexander <sany199574@gmail.com>
2023-12-29 00:02:11 +0300

    Добавлена модель  - User

---
+++ src/Test_workshop/Rabbit_mq_Project/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Rabbit_mq_Project.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
