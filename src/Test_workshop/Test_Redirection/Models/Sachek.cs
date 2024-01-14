c14905b9b6636224e4c713016db67a25afa2ba03
Alexander <sany199574@gmail.com>
2024-01-15 00:01:19 +0300

    Удалён не используемый класс

--- src/Test_workshop/Test_Redirection/Models/Sachek.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Test_Redirection.Models
-{
-    public class Sachek
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public List<string> Message { get; set; }
-    }
-}
