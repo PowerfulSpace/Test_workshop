da283938cb645a5d7e9ab3b91e8de45c81d7a50e
Alexander <sany199574@gmail.com>
2024-01-18 01:08:01 +0300

    Добавлен метод  - WordDivisions

---
+++ src/Test_workshop/Test_regular/Metods/RegularMethods.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Test_regular.Metods
+{
+    public static class RegularMethods
+    {
+
+        public static void WordDivisions()
+        {
+            string text = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
+
+            Regex regex = new Regex(@"[а-яА-Я]\w*\D?\s?");
+
+            MatchCollection result = regex.Matches(text);
+
+            if (result.Count > 0)
+            {
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+
+    }
+}
---
+++ src/Test_workshop/Test_regular/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Text.RegularExpressions;
+
+
+
+
+string text = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
+
+Regex regex = new Regex(@"[а-яА-Я]\w*\D?\s?");
+
+MatchCollection result = regex.Matches(text);
+
+if (result.Count > 0)
+{
+    foreach (var item in result)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+
+
+Console.ReadLine();
