8b243a7871386bb4ab11b8f8989f19055780891c
Alexander <sany199574@gmail.com>
2024-01-18 22:16:15 +0300

    Добавлен метод вычитание из группы  - SubtractFromTheGroup

--- src/Test_workshop/Test_regular/Metods/RegularMethods.cs
+++ src/Test_workshop/Test_regular/Metods/RegularMethods.cs
@@ -20,6 +20,21 @@ namespace Test_regular.Metods
                     Console.WriteLine(item);
                 }
             }
+        } 
+        public static void SubtractFromTheGroup()
+        {
+
+            //Вычитаем из группы от 0-9 все чётные цифры 2468
+            string[] inputs = { "123", "13579753", "3557798", "335599901" };
+            string pattern = @"^[0-9-[2468]]+$";
+
+            foreach (string input in inputs)
+            {
+                Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                    Console.WriteLine(match.Value);
+            }
+
         }
 
 
--- src/Test_workshop/Test_regular/Program.cs
+++ src/Test_workshop/Test_regular/Program.cs
@@ -1,23 +1,15 @@
-﻿
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 
+//Вычитаем из группы от 0-9 все чётные цифры 2468
+string[] inputs = { "123", "13579753", "3557798", "335599901" };
+string pattern = @"^[0-9-[2468]]+$";
 
-
-string text = "Бык тупогуб, тупогубенький бычок, у быка губа бела была тупа";
-
-Regex regex = new Regex(@"[а-яА-Я]\w*\D?\s?");
-
-MatchCollection result = regex.Matches(text);
-
-if (result.Count > 0)
+foreach (string input in inputs)
 {
-    foreach (var item in result)
-    {
-        Console.WriteLine(item);
-    }
+    Match match = Regex.Match(input, pattern);
+    if (match.Success)
+        Console.WriteLine(match.Value);
 }
-
-
 
 Console.ReadLine();
