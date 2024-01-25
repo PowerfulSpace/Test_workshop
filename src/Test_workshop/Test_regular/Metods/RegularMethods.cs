19f1dba7e45ed0cb22ca8b535222887009a938ab
Alexander <sany199574@gmail.com>
2024-01-26 00:06:42 +0300

    Временно закментированный текст для, дальнейшего улучшения

--- src/Test_workshop/Test_regular/Metods/RegularMethods.cs
+++ src/Test_workshop/Test_regular/Metods/RegularMethods.cs
@@ -25,15 +25,15 @@ namespace Test_regular.Metods
         {
 
             //Вычитаем из группы от 0-9 все чётные цифры 2468
-            string[] inputs = { "123", "13579753", "3557798", "335599901" };
-            string pattern = @"^[0-9-[2468]]+$";
+            //string[] inputs = { "123", "13579753", "3557798", "335599901" };
+            //string pattern = @"^[0-9-[2468]]+$";
 
-            foreach (string input in inputs)
-            {
-                Match match = Regex.Match(input, pattern);
-                if (match.Success)
-                    Console.WriteLine(match.Value);
-            }
+            //foreach (string input in inputs)
+            //{
+            //    Match match = Regex.Match(input, pattern);
+            //    if (match.Success)
+            //        Console.WriteLine(match.Value);
+            //}
 
         }
 
