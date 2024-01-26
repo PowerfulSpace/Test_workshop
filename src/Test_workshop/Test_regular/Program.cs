2129da5b32de28da0c4e804ab1c7c5dd6cdae634
Alexander <sany199574@gmail.com>
2024-01-27 01:16:46 +0300

    Добавлен пример, извлекающий только цифры из номера

--- src/Test_workshop/Test_regular/Program.cs
+++ src/Test_workshop/Test_regular/Program.cs
@@ -1,1 +1,9 @@
 ﻿
+using System.Text.RegularExpressions;
+
+string phoneNumber = "+1(876)-234-12-98";
+
+Regex regex = new Regex(@"\D");
+string result = regex.Replace(phoneNumber, "");
+
+Console.WriteLine(result);
