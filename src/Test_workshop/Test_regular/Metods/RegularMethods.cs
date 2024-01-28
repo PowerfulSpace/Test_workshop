8e8e1c8f42645de91867b831305a40bcfa036601
Alexander <sany199574@gmail.com>
2024-01-29 00:26:58 +0300

    Удалён метод  - Multiline

--- src/Test_workshop/Test_regular/Metods/RegularMethods.cs
+++ src/Test_workshop/Test_regular/Metods/RegularMethods.cs
@@ -22,31 +22,6 @@ namespace Test_regular.Metods
             }
         } 
 
-        public static void Multiline()
-        {
-            SortedList<int, string> scores = new SortedList<int, string>(new DescendingComparer<int>());
-
-            string input = "Joe 164\n" +
-                           "Sam 208\n" +
-                           "Allison 211\n" +
-                           "Gwen 171\n";
-            string pattern = @"(?m)^(\w+)\s(\d+)\r*$";
-
-            foreach (Match match in Regex.Matches(input, pattern, RegexOptions.Multiline))
-                scores.Add(Convert.ToInt32(match.Groups[2].Value), match.Groups[1].Value);
-
-            // List scores in descending order.
-            foreach (KeyValuePair<int, string> score in scores)
-                Console.WriteLine("{0}: {1}", score.Value, score.Key);
-
-
-            //Преобразует текст в одну строку
-            string pattern2 = "(?s)^.+";
-            string input2 = "This is one line and" + Environment.NewLine + "this is the second.";
-
-            foreach (Match match in Regex.Matches(input2, pattern2))
-                Console.WriteLine(Regex.Escape(match.Value));
-        }
 
         public class DescendingComparer<T> : IComparer<T>
         {
