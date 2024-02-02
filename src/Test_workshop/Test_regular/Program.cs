86da728b4f97154b294701de037c71ed272bfe10
Alexander <sany199574@gmail.com>
2024-02-02 04:12:40 +0300

    Удалён пример  - Демонстративно показано как рабоатет ленивое совпадение

--- src/Test_workshop/Test_regular/Program.cs
+++ src/Test_workshop/Test_regular/Program.cs
@@ -13,21 +13,7 @@ foreach (var item in result)
 
 Console.WriteLine();
 
-//Демонстративно показано как рабоатет ленивое совпадение
-string pattern = @"^\s*(System.)??Console.Write(Line)??\(??";
-string input = "System.Console.WriteLine(\"Hello!\")\n" +
-                      "Console.Write(\"Hello!\")\n" +
-                      "Console.WriteLine(\"Hello!\")\n" +
-                      "Console.ReadLine()\n" +
-                      "   Console.WriteLine";
 
-Console.WriteLine();
-foreach (Match match in Regex.Matches(input, pattern,
-                                      RegexOptions.IgnorePatternWhitespace |
-                                      RegexOptions.IgnoreCase |
-                                      RegexOptions.Multiline))
-    Console.WriteLine("'{0}' found at position {1}.", match.Value, match.Index);
-Console.WriteLine();
 
 
 
