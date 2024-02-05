5ab584a4e163a8eed3581daaa793e5f4dd96581d
Alexander <sany199574@gmail.com>
2024-02-05 12:10:37 +0300

    Решил задачу про градусы между стрелками

---
+++ src/Test_workshop/ClockFace/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+var result = Test(3, 30);
+
+Console.WriteLine(result);
+
+Console.ReadLine();
+
+
+
+
+static int Test(int hour, int minutes)
+{
+    int degree = 6;
+    int x =  hour * 5 * degree;
+    int y = minutes * degree;
+    int result = 0;
+
+    if(x > y)
+    {
+        result = x - y;
+    }
+    else
+    {
+        result = y - x;
+    }
+
+    return result;
+}
+
+
