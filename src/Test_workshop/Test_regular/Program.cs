7762db785c391bc33b93bb1082af7fac2391c3ac
Alexander <sany199574@gmail.com>
2024-02-08 10:25:27 +0300

    Очищен не используемый код, из-за которого возникают ошибки

--- src/Test_workshop/Test_regular/Program.cs
+++ src/Test_workshop/Test_regular/Program.cs
@@ -1,10 +1,3 @@
 ﻿
-using Test_regular.Metods;
-
-await AddTakeDemo.BC_AddTakeCompleteAdding();
-TryTakeDemo.BC_TryTake();
-FromToAnyDemo.BC_FromToAny();
-await ConsumingEnumerableDemo.BC_GetConsumingEnumerable();
-Console.WriteLine("Press any key to exit.");
 
 Console.ReadLine();
