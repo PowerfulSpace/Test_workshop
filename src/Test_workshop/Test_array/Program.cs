62cbc8ec27aa98213adf3b1bebacb8d39eba55b5
Alexander <sany199574@gmail.com>
2024-02-14 03:22:02 +0300

    Добавлены 2 примера для последущего обьяснения решения

---
+++ src/Test_workshop/Test_array/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int[] array = new int[3] { 10, 15, 20 };
+
+
+Method(array);
+
+Console.WriteLine(array[0]);
+
+Console.ReadLine();
+
+
+
+static void Method(int[] array)
+{
+    array = new int[3];
+    array[0] = 25;
+}
+
+
+
+//Пример 1
+
+//int[] array = new int[3] { 10, 15, 20 };
+
+
+//Method(array);
+
+//Console.WriteLine(array[0]);
+
+//Console.ReadLine();
+
+
+
+//static void Method(int[] array)
+//{
+//    array[0] = 25;
+//}
