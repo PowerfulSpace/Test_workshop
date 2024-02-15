2d8304972549cfebe9b8fd54564946468c0d1be4
Alexander <sany199574@gmail.com>
2024-02-15 18:49:37 +0300

    Добавлен пример из нововведения

--- src/Test_workshop/Test_array/Program.cs
+++ src/Test_workshop/Test_array/Program.cs
@@ -1,6 +1,20 @@
 ﻿
 
-Console.WriteLine("{0:#### (###-##-##)}",80293190884);
+void Sum(params int[] numbers)
+{
+    int result = 0;
+    foreach (var n in numbers)
+    {
+        result += n;
+    }
+    Console.WriteLine(result);
+}
+
+int[] nums = { 1, 2, 3, 4, 5 };
+Sum(nums);
+Sum(1, 2, 3, 4);
+Sum(1, 2, 3);
+Sum();
 
 Console.ReadLine();
 
