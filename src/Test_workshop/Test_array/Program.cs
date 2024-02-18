afdec2490c8800afb51187d42388325f7c412196
Alexander <sany199574@gmail.com>
2024-02-19 00:15:57 +0300

    Добавлен пример, интересное новвоведение Значения по умолчанию для параметров лямбда-выражений

--- src/Test_workshop/Test_array/Program.cs
+++ src/Test_workshop/Test_array/Program.cs
@@ -13,8 +13,11 @@ int[] nums2 = new int[] { };   // пустой массив
 
 
 
- 
 
+var welcome = (string message = "hello") => Console.WriteLine(message);
+
+welcome("hello world"); // hello world
+welcome();              // hello
 
 
 Console.ReadLine();
