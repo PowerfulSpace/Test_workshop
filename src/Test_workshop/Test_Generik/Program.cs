d39ee3e61a655081cdc1ecb3b175ea5d59fb5a92
Alexander <sany199574@gmail.com>
2024-02-20 00:06:43 +0300

    Добавлен вывод информации

--- src/Test_workshop/Test_Generik/Program.cs
+++ src/Test_workshop/Test_Generik/Program.cs
@@ -3,6 +3,11 @@
 Person<int> tom = new Person<int>(546, "Tom");
 Company<Person<int>> microsoft = new Company<Person<int>>(tom);
 
+Console.WriteLine(microsoft.CEO.Id);  // 546
+Console.WriteLine(microsoft.CEO.Name);  // Tom
+
+
+Console.ReadLine();
 
 // класс компании
 class Company<P>
