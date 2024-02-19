f5acf668888a001be8492181a57947dce4bc3d13
Alexander <sany199574@gmail.com>
2024-02-20 00:05:23 +0300

    Добавлен класс Company, с егопримером реализаций

--- src/Test_workshop/Test_Generik/Program.cs
+++ src/Test_workshop/Test_Generik/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+// класс компании
+class Company<P>
+{
+    public P CEO { get; set; }  // президент компании
+    public Company(P ceo)
+    {
+        CEO = ceo;
+    }
+}
+
