8a1eef53ebaefd08ea2f4fd8e6242b11bea982b4
Alexander <sany199574@gmail.com>
2024-02-23 00:24:13 +0300

    Добавлен пример, абстрактных свойств, и его правильная реализация

--- src/Test_workshop/Test_Abstract/Program.cs
+++ src/Test_workshop/Test_Abstract/Program.cs
@@ -36,3 +36,23 @@ interface Test2
 
 
 
+abstract class Transport
+{
+    // абстрактное свойство для хранения скорости
+    public abstract int Speed { get; set; }
+}
+// класс корабля
+class Ship : Transport
+{
+    int speed;
+    public override int Speed
+    {
+        get => speed;
+        set => speed = value;
+    }
+}
+
+class Aircraft : Transport
+{
+    public override int Speed { get; set; }
+}
