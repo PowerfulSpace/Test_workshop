514e5f3d0f6619ad9b9488c9b9e5589a4dac51cc
Alexander <sany199574@gmail.com>
2024-02-27 18:27:34 +0300

    Добавлен пример на внимательнсоть, проблема в модификаторе доступа

--- src/Test_workshop/Test_Questions/Program.cs
+++ src/Test_workshop/Test_Questions/Program.cs
@@ -1,9 +1,33 @@
 ﻿
 
+//B obj1 = new A();
+//obj1.Foo();
 
+B obj2 = new B();
+obj2.Foo();
+
+A obj3 = new B();
+obj3.Foo();
 
 
 
 
 
 Console.ReadLine();
+
+
+
+class A
+{
+    virtual void Foo()
+    {
+        Console.Write("Class A");
+    }
+}
+class B : A
+{
+    override void Foo()
+    {
+        Console.Write("Class B");
+    }
+}
