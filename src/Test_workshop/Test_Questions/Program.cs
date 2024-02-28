9c528402d1de0eb6384a245c21ae3e374412b8d4
Alexander <sany199574@gmail.com>
2024-02-28 11:24:36 +0300

    Добавлен пример с отловом ошибок

--- src/Test_workshop/Test_Questions/Program.cs
+++ src/Test_workshop/Test_Questions/Program.cs
@@ -1,10 +1,24 @@
 ﻿
 
 
-var a = null;
-a = 10;
-Console.WriteLine(a);
-Console.ReadLine();
+
+try
+{
+    var array = new int[] { 1, 2 };
+    Console.Write(array[5]);
+}
+catch (ApplicationException e)
+{
+    Console.Write(1);
+}
+catch (SystemException e)
+{
+    Console.Write(2);
+}
+catch (Exception e)
+{
+    Console.Write(3);
+}
 
 Console.ReadLine();
 
