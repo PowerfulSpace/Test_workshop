d418744889d057e1c3dffca402a8626e8e1994ea
Alexander <sany199574@gmail.com>
2024-03-01 01:16:56 +0300

    Удалён не испольуемый код

--- src/Test_workshop/Test_Questions/Program.cs
+++ src/Test_workshop/Test_Questions/Program.cs
@@ -1,53 +1,6 @@
 ﻿
-Person tom = new Person();
-tom.Print();    // Имя:Tom  Возраст: 1
-
-
-var test = new Test();
-try
-{
-    test.Print();
-}
-catch (Exception)
-{
-    Console.Write("5");
-}
-finally
-{
-    Console.Write("4");
-}
 
 
 
 Console.ReadLine();
 
-
-
-class Test
-{
-    public void Print()
-    {
-        try
-        {
-            throw new Exception();
-        }
-        catch (Exception)
-        {
-            Console.Write("9");
-            throw new Exception();
-        }
-        finally
-        {
-            Console.Write("2");
-        }
-    }
-}
-
-struct Person
-{
-    // инициализация полей значениями по умолчанию - доступна с C#10
-    public string name = "Tom";
-    public int age = 1;
-    public Person() { }
-    public void Print() => Console.WriteLine($"Имя: {name}  Возраст: {age}");
-}
