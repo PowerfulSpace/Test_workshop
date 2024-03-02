c0f5b5d75916fd696b05803ac91bed22e42afe49
Alexander <sany199574@gmail.com>
2024-03-02 16:27:15 +0300

    Удалена не используемая часть кода

--- src/Test_workshop/Test_Funny/Program.cs
+++ src/Test_workshop/Test_Funny/Program.cs
@@ -18,20 +18,7 @@ class Класс
 
     public void Metoд(int[] a)
     {
-        if (a.Length > 0)
-        {
-            if (a[0] == 1)
-            {
-                Console.WriteLine("Элемент равен 1 в массиве");
-            }
-            else if (a[0] == 2)
-            {
-                Console.WriteLine("Элемент равен 2 в массиве");
-            }
-            else
-            {
-                Console.WriteLine("Ни чего не знаю");
-            }
-        }
+       
+
     }
 }
