8f3c2504f55ddc05a7773ed21a36dcdba5577311
Alexander <sany199574@gmail.com>
2024-03-04 01:10:33 +0300

    Удалено задание с факториалом

--- src/Test_workshop/Test_Questions/Program.cs
+++ src/Test_workshop/Test_Questions/Program.cs
@@ -1,20 +1,5 @@
 ﻿
-int Factorial(int n)
-{
-    if (n == 1) return 1;
-
-    return n * Factorial(n - 1);
-}
-
-int factorial4 = Factorial(4);  // 24
-int factorial5 = Factorial(5);  // 120
-int factorial6 = Factorial(6);  // 720
-
-Console.WriteLine($"Факториал числа 4 = {factorial4}");
-Console.WriteLine($"Факториал числа 5 = {factorial5}");
-Console.WriteLine($"Факториал числа 6 = {factorial6}");
 
 
 Console.ReadLine();
 
-//Дописать ещё вариант 
