f6a030114627ad746ec5026234aef2ae321da1b1
Alexander <sany199574@gmail.com>
2024-03-05 02:27:33 +0300

    Удалена пароллельная операция

--- src/Test_workshop/Test_Abstract/Program.cs
+++ src/Test_workshop/Test_Abstract/Program.cs
@@ -1,15 +1,5 @@
 ﻿
 
-int cnt = 0;
-var list = Enumerable.Range(0, 10000).ToList();
-var options = new ParallelOptions() { MaxDegreeOfParallelism = 8 };
-Parallel.ForEach(list, options, (element) =>
-{
-
-    var localCnt = Interlocked.Increment(ref cnt);
-    if (localCnt != cnt)
-        Console.WriteLine("{0} {1} {2}", cnt, localCnt, Thread.CurrentThread.ManagedThreadId);
-});
 
 
 
