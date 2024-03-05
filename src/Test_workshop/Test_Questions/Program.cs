e6d80b1699092dbb1527a5ef412f87a271a13896
Alexander <sany199574@gmail.com>
2024-03-05 13:49:36 +0300

    Добавлена асбтрактная версия примера

--- src/Test_workshop/Test_Questions/Program.cs
+++ src/Test_workshop/Test_Questions/Program.cs
@@ -1,5 +1,43 @@
 ﻿
 
 
+using System;
+
 Console.ReadLine();
 
+abstract class TestClass
+{
+    Person[] personal;
+    private int IdA;
+    abstract private int IdB;
+
+    public int MyPropertyA { get; set; }
+    abstract public int MyPropertyB { get; set; }
+
+
+    public TestClass(Person[] people) => personal = people;
+
+
+
+    public TestClass this[int index]
+    {
+        get => personal[index];
+        set => personal[index] = value;
+    }
+
+    
+   
+
+    public void MethodA()
+    {
+
+    }
+
+    abstract public void MethodB();
+}
+
+class Person
+{
+    public string Name { get; }
+    public Person(string name) => Name = name;
+}
