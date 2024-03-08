371e91e4c402241b3ae2950eb54c8d070c374c18
Alexander <sany199574@gmail.com>
2024-03-09 00:19:19 +0300

    Удалён не используемый класс

--- src/Test_workshop/Test_Abstract/Program.cs
+++ src/Test_workshop/Test_Abstract/Program.cs
@@ -10,20 +10,7 @@ Console.ReadLine();
 
 
 
-interface Test2
-{
-    public static int MyProperty { get; set; }
 
-
-
-
-   
-
-    //public readonly int value;
-
-    public abstract void Method();
-
-}
 
 
 
