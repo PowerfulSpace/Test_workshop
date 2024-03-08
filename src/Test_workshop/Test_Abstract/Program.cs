a77efa8277a616fdd1f9b08066c838e0f1df9d8b
Alexander <sany199574@gmail.com>
2024-03-09 00:18:46 +0300

    Удалено не используемое свойство

--- src/Test_workshop/Test_Abstract/Program.cs
+++ src/Test_workshop/Test_Abstract/Program.cs
@@ -18,7 +18,7 @@ interface Test2
     protected int MyProperty3 { get; set; }
     private protected int MyProperty4 { get; set; }
     internal int MyProperty5 { get; set; }
-    protected internal int MyProperty6 { get; set; }
+   
 
     //public readonly int value;
 
