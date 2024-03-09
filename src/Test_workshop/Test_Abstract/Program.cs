e646cc4efd7607535f26af40e5185c6b6b6fe707
Alexander <sany199574@gmail.com>
2024-03-10 00:04:48 +0300

    Добавлен класс Test2

--- src/Test_workshop/Test_Abstract/Program.cs
+++ src/Test_workshop/Test_Abstract/Program.cs
@@ -10,7 +10,21 @@ Console.ReadLine();
 
 
 
+interface Test2
+{
+    public static int MyProperty { get; set; }
+    public int MyProperty1 { get; set; }
+    //private int MyProperty2 { get; set; }
+    protected int MyProperty3 { get; set; }
+    private protected int MyProperty4 { get; set; }
+    internal int MyProperty5 { get; set; }
+    protected internal int MyProperty6 { get; set; }
 
+    //public readonly int value;
+
+    public abstract void Method();
+
+}
 
 
 
