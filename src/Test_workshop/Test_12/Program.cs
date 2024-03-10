811171f9dcc321ad8ae3169f13714baf28370e6c
Alexander <sany199574@gmail.com>
2024-03-11 00:04:21 +0300

    Добавлен пример с использованием конструктура новой версии

---
+++ src/Test_workshop/Test_12/Program.cs
@@ -0,0 +1,13 @@
+﻿var tom = new Person("Tom", 38);
+Console.WriteLine(tom);
+
+Console.ReadLine();
+
+public class Person(string name, int age)
+{
+    public Person(string name) : this(name, 18) { }
+    public string Name => name;
+    public int Age => age;
+
+    public override string ToString() => $"name: {name}, age: {age}";
+}
