47b3671e32c1c98bc6572ca448f9bdf4bcbd7413
Alexander <sany199574@gmail.com>
2024-03-12 00:53:47 +0300

    Удалён не используемый код

--- src/Test_workshop/Test_12/Program.cs
+++ src/Test_workshop/Test_12/Program.cs
@@ -1,13 +1,7 @@
-﻿var tom = new Person("Tom", 38);
-Console.WriteLine(tom);
+﻿
+
+
+
+
 
 Console.ReadLine();
-
-public class Person(string name, int age)
-{
-    public Person(string name) : this(name, 18) { }
-    public string Name => name;
-    public int Age => age;
-
-    public override string ToString() => $"name: {name}, age: {age}";
-}
