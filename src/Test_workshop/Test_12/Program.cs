a1c0b1786d1aacc829dc47a78575a89e8358736e
Alexander <sany199574@gmail.com>
2024-03-12 00:57:03 +0300

    Добавлен пример использование паатерна массива

--- src/Test_workshop/Test_12/Program.cs
+++ src/Test_workshop/Test_12/Program.cs
@@ -1,7 +1,17 @@
 ﻿
+Console.WriteLine(GetNumber(new[] { 2, 3, 5 }));        // 1
+Console.WriteLine(GetNumber(new[] { 2, 4, 5 }));        // 2
+Console.WriteLine(GetNumber(new[] { 2, 3, 4, 5 }));     // 3
+Console.WriteLine(GetNumber(new[] { 1, 2, 3 }));        // 4
+Console.WriteLine(GetNumber(new int[] { }));            // 4
 
-
-
+int GetNumber(int[] values) => values switch
+{
+    [2, 3, 5] => 1,
+    [2, _, 5] => 2,
+    [2, .., 5] => 3,
+    [..] => 4
+};
 
 
 Console.ReadLine();
