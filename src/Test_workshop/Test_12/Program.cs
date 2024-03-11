3e474624c3e34d8cba4ba73982633d3a5fc4bbca
Alexander <sany199574@gmail.com>
2024-03-12 00:59:40 +0300

    Заменён пример

--- src/Test_workshop/Test_12/Program.cs
+++ src/Test_workshop/Test_12/Program.cs
@@ -1,29 +1,32 @@
 ﻿
-Console.WriteLine(GetNumber(new[] { 2, 3, 5 }));        // 1
-Console.WriteLine(GetNumber(new[] { 2, 4, 5 }));        // 2
-Console.WriteLine(GetNumber(new[] { 2, 3, 4, 5 }));     // 3
-Console.WriteLine(GetNumber(new[] { 1, 2, 3 }));        // 4
-Console.WriteLine(GetNumber(new int[] { }));            // 4
 
-int GetNumber(int[] values) => values switch
+Print();
+PrintValue("hello");
+
+void Print()
 {
-    [2, 3, 5] => 1,
-    [2, _, 5] => 2,
-    [2, .., 5] => 3,
-    [..] => 4
-};
+    string text = """
+              <element attr="content">
+                <body>
+                </body>
+              </element>
+              """;
+    Console.WriteLine(text);
+}
+
+void PrintValue(string val)
+{
+    string text = $"""
+              <element attr="content">
+                <body>
+                {val}
+                </body>
+              </element>
+              """;
+    Console.WriteLine(text);
+}
+
 
 
 Console.ReadLine();
 
-class C
-{
-    [Reader<int>]
-    public void M()
-    {
-    }
-}
-
-class ReaderAttribute<T> : Attribute
-{
-}
