dd0c2a98022910a72f51363972fefa21d76b321a
Alexander <sany199574@gmail.com>
2024-03-13 00:06:12 +0300

    Удалён класс Person

--- src/Test_workshop/Test_12/Program.cs
+++ src/Test_workshop/Test_12/Program.cs
@@ -1,30 +1,5 @@
 ﻿
 
-Print();
-PrintValue("hello");
-
-void Print()
-{
-    string text = """
-              <element attr="content">
-                <body>
-                </body>
-              </element>
-              """;
-    Console.WriteLine(text);
-}
-
-void PrintValue(string val)
-{
-    string text = $"""
-              <element attr="content">
-                <body>
-                {val}
-                </body>
-              </element>
-              """;
-    Console.WriteLine(text);
-}
 
 
 
