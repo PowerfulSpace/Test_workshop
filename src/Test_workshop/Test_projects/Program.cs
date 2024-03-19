3e784f9b5ac1d116cb6b0d22fc382d9c49647c4b
Alexander <sany199574@gmail.com>
2024-03-20 00:23:19 +0300

    Добавлен пример значений по дефолду для лямд

--- src/Test_workshop/Test_projects/Program.cs
+++ src/Test_workshop/Test_projects/Program.cs
@@ -3,6 +3,12 @@ int number = 5;
 Increment(ref number);
 Console.WriteLine($"Число после метода Increment: {number}");
 
+var welcome = (string message = "hello") => Console.WriteLine(message);
+
+welcome("hello world"); // hello world
+welcome();              // hello
+
+
 Console.ReadLine();
 
 
