abb59e7acefe4d053a2809860c110d07977343d5
Alexander <sany199574@gmail.com>
2024-03-22 05:25:14 +0300

    удален не используемый метод

--- src/Test_workshop/Test_projects/Program.cs
+++ src/Test_workshop/Test_projects/Program.cs
@@ -1,7 +1,4 @@
 ﻿
-int number = 5;
-Increment(ref number);
-Console.WriteLine($"Число после метода Increment: {number}");
 
 var welcome = (string message = "hello") => Console.WriteLine(message);
 
@@ -9,10 +6,3 @@ welcome("hello world"); // hello world
 welcome();              // hello
 
 Console.ReadLine();
-
-
-void Increment(ref readonly int n)
-{
-    // n++; // нельзя, иначе будет очишка компиляции
-    Console.WriteLine($"Число в методе Increment: {n}");
-}
