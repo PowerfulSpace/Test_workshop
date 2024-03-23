aa657b8b3513a97c7e9f386f96417d36299c3833
Alexander <sany199574@gmail.com>
2024-03-23 04:24:48 +0300

    Добавлен метод GetNumber

--- src/Test_workshop/Test_projects/Program.cs
+++ src/Test_workshop/Test_projects/Program.cs
@@ -5,4 +5,23 @@ var welcome = (string message = "hello") => Console.WriteLine(message);
 welcome("hello world"); // hello world
 welcome();              // hello
 
+
+
+
+Console.WriteLine(GetNumber(new[] { 2, 3, 5 }));        // 1
+Console.WriteLine(GetNumber(new[] { 2, 4, 5 }));        // 2
+Console.WriteLine(GetNumber(new[] { 2, 3, 4, 5 }));     // 3
+Console.WriteLine(GetNumber(new[] { 1, 2, 3 }));        // 4
+Console.WriteLine(GetNumber(new int[] { }));            // 4
+
+
 Console.ReadLine();
+
+
+int GetNumber(int[] values) => values switch
+{
+    [2, 3, 5] => 1,
+    [2, _, 5] => 2,
+    [2, .., 5] => 3,
+    [..] => 4
+};
