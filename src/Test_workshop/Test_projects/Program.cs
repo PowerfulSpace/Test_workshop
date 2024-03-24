3327351c0493cc1302ef78a8925f997341ea3ff5
Alexander <sany199574@gmail.com>
2024-03-24 05:32:35 +0300

    Полностью отищен пример

--- src/Test_workshop/Test_projects/Program.cs
+++ src/Test_workshop/Test_projects/Program.cs
@@ -1,25 +1,5 @@
 ﻿
 
-var welcome = (string message = "hello") => Console.WriteLine(message);
-
-welcome("hello world"); // hello world
-welcome();              // hello
-
-
-
-
-Console.WriteLine(GetNumber(new[] { 2, 3, 5 }));        // 1
-Console.WriteLine(GetNumber(new[] { 2, 4, 5 }));        // 2
-Console.WriteLine(GetNumber(new[] { 2, 3, 4, 5 }));     // 3
-Console.WriteLine(GetNumber(new[] { 1, 2, 3 }));        // 4
-Console.WriteLine(GetNumber(new int[] { }));            // 4
-
-Print();
-PrintValue("hello");
-
-
-
-
 
 Console.ReadLine();
 
