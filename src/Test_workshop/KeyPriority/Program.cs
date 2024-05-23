f7de8ce3a404c2a1bcc8d3ada859e449ebe1ff38
Alexander <sany199574@gmail.com>
2024-05-23 21:00:56 +0300

    Добавлен алгоритм по сортировки значений словаря

---
+++ src/Test_workshop/KeyPriority/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+Dictionary<char, int> keys = new Dictionary<char, int>
+{
+    {'5',2 },
+    {'3',2 },
+    {'7',2 },
+    {'6',4 },
+    {'1',2 },
+    {'9',3 },
+    {'8',4 },
+    {'4',1 },
+    {'2',1 },
+};
+
+GetPriority(keys);
+
+
+
+
+
+Console.ReadLine();
+
+
+static List<char> GetPriority(Dictionary<char, int> keys)
+{
+    var list = keys.OrderBy(x => x.Value);
+
+    List<char> list2 = list.Select(x => x.Key).ToList();
+    list2.Reverse();
+    return list2;
+}
+
